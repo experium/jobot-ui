0bb4073e8739ded98cab5f12096e36f51185b369
Dmitry Tkachuk <dev.trauret@gmail.com>
2018-12-06 13:15:20 +0300

    Fix props warnings with styled smooth-ui components (#4)

--- src/ui/components/Select.js
+++ src/ui/components/Select.js
@@ -1,12 +1,18 @@
+import React from 'react';
 import { styled, Select as SmoothSelect, css } from '@smooth-ui/core-sc'
 
-export const Select = styled(SmoothSelect)`
-        select{
+export const Select = ({ icon, ...props }) => {
+    const Component = styled(SmoothSelect)`
+        select {
             border-radius: 0;
         }
-    ${p => p.icon && css`
-        select{
-            padding-left: 40px;
-        }
-    `}
-`;
+
+        ${icon && css`
+            select {
+                padding-left: 40px;
+            }
+        `}
+    `;
+
+    return <Component {...props} />;
+};
--- src/ui/components/Tag.js
+++ src/ui/components/Tag.js
@@ -1,19 +1,24 @@
+import React from 'react';
 import { styled , Box, css } from "@smooth-ui/core-sc";
 
-export const Tag = styled(Box)`
-    cursor: ${props => props.link === 'true' ? 'pointer' : 'inherit'};
-    &:hover{
-            ${p => p.link && css`
-                color: #FF4F57;
-            `}
+export const Tag = ({ link, ...props }) => {
+    const Component = styled(Box)`
+        cursor: ${link === 'true' ? 'pointer' : 'inherit'};
+        &:hover {
+                ${link && css`
+                    color: #FF4F57;
+                `}
+            }
+        svg {
+            margin-right: 5px;
+            width: 16px;
+            position: relative;
+            top: -2px;
         }
-    svg{
-        margin-right: 5px;
-        width: 16px;
-        position: relative;
-        top: -2px;
-    }
-`;
+    `;
+
+    return <Component {...props} />;
+};
 
 Tag.defaultProps = {
     display: 'inline-block',
