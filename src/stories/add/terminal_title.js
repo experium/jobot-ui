eb96a2017094a6c805728dcb594477fd75ffaefa
Роман Яковенко <bidtiz@gmail.com>
2019-02-19 10:47:58 +0300

    add title component

--- src/stories/add/terminal_title.js
+++ src/stories/add/terminal_title.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 import {
     Alert, Box, Typography,
-    TerminalTitle
+    TerminalTitle, TerminalSubTitle,
+    TerminalOrText
 } from '../ui';
 
 const TerminalComponentTitle = () => ( <Box p={10}>
@@ -10,11 +11,15 @@ const TerminalComponentTitle = () => ( <Box p={10}>
         <Typography variant="h3" mt={20}>How to Use</Typography>
         <Alert variant="info">Support <a href="https://smooth-ui.smooth-code.com/docs-components-typography#api">API component <b>Typography</b> smooth-ui</a></Alert>
         <code>
-<pre>{`import { TerminalTitle } from `}<Box color="red" display="inline-block">'@experium/jobot-ui';</Box><br/>{`
+<pre>{`import { TerminalTitle, TerminalSubTitle, TerminalOrText } from `}<Box color="red" display="inline-block">'@experium/jobot-ui';</Box><br/>{`
 <TerminalTitle>Title</TerminalTitle>
+<TerminalSubTitle>Sub Title</TerminalSubTitle>
+<TerminaOrText><span>Or Title</span></TerminaOrText>
 `}</pre>
         </code>
         <TerminalTitle>Title</TerminalTitle>
+        <TerminalSubTitle mode='single' max={32}>Sub Title</TerminalSubTitle>
+        <TerminalOrText><span>Or Title</span></TerminalOrText>
     </Box>
 );
 
--- src/ui/components/TerminalTitle.js
+++ src/ui/components/TerminalTitle.js
@@ -1,7 +1,45 @@
-import { styled, Typography } from "@smooth-ui/core-sc";
+import { styled, Typography, Box } from "@smooth-ui/core-sc";
+import { Textfit } from 'react-textfit';
 
 export const TerminalTitle = styled(Typography)``;
 
 TerminalTitle.defaultProps = {
     variant: 'display-3'
+};
+export const TerminalSubTitle = styled(Textfit)`
+    color: #444c63;
+    font-size: 32px;
+    font-weight: 600;
+    margin-bottom: 1.5rem;
+    text-transform: uppercase;
+`;
+export const TerminalOrText = styled(Box)`
+    overflow: hidden;
+    text-align: center;
+    color: #444c63;
+    &:after,&:before{
+        content: "";
+        display: inline-block;
+        vertical-align: middle;
+        width: 100%;
+        height: 1px;
+        background-color: #dfdfdf;
+        position: relative;
+    }
+    &:before{
+        margin-left: -100%;
+        left: -14px;
+    }
+    &:after{
+        margin-right: -100%;
+        right: -14px;
+    }
+    span{
+        display: inline-block;
+        vertical-align: middle;
+    }
+`;
+TerminalOrText.defaultProps = {
+    mode: 'single',
+    max: 32
 };
