063aadc02ae864277d98b76768e9c7e0f709097d
Nastasia <muaaaaaaa@mail.ru>
2019-05-31 11:10:45 +0300

    fix input

--- src/ui/components/Input.js
+++ src/ui/components/Input.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { styled, Input as SmoothInput, css } from '@smooth-ui/core-sc';
 
-export const Input = ({ icon, ...props }) => {
-    const Component = styled(SmoothInput)`
-        border-radius: 0;
-        ${icon && css`
-            padding-left: 40px;
-        `}
-    `;
-
-    return <Component {...props} />;
-};
+export const Input = styled(SmoothInput)`
+    border-radius: 0;
+    ${({ icon }) => icon && css`
+        padding-left: 40px;
+    `}
+`;
 
 Input.defaultProps = {
     position: 'relative',
